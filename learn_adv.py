bfac85f9c5f933702353195eb147e559b27e005a
Tianjiao Wang <wtj327@gmail.com>
2020-10-25 17:57:55 -0400

    add main.py

---
+++ learn_adv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Python Advanced Features
+
+def test_code():
+    # Slice
+    names = ['Tianjiao', 'Tao', 'Taylor']
+    names_2 = names[0:2]
+    print(names_2)
+
+    
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import learn_basics
+import learn_functions
+import learn_adv
+
+learn_basics.test_code()
+learn_functions.test_code()
+learn_adv.test_code()
